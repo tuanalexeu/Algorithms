20febe9dc7034d07e31ca12f82e5ce8f26c45c21
AlexeyTyan <alekseytyan45@gmail.com>
2021-01-28 11:19:07 +0300

    Tree: Implement search methods (Iterative & Recursive)

--- src/com/company/datastructures/MyTree.java
+++ src/com/company/datastructures/MyTree.java
@@ -2,21 +2,44 @@ package com.company.datastructures;
 
 public class MyTree {
     MyTree left, right;
-    int value;
+    int key;
 
 
-    public MyTree(MyTree left, MyTree right, int value) {
+    public MyTree(MyTree left, MyTree right, int key) {
         this.left = left;
         this.right = right;
-        this.value = value;
+        this.key = key;
     }
 
-    public MyTree(int value) {
-        this.value = value;
+    public MyTree(int key) {
+        this.key = key;
+    }
+
+    public MyTree searchRecursive(MyTree node, int key) {
+        if(node == null || key == node.key) {
+            return node;
+        }
+
+        if(key < node.key) {
+            return searchRecursive(node.left, key);
+        } else {
+            return searchRecursive(node.right, key);
+        }
+    }
+
+    public MyTree searchIterative(MyTree node, int key) {
+        while(node != null && key != node.key) {
+            if(key < node.key) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return node;
     }
 
     public static int sumRecursive(MyTree root) {
-        int sum = root.value;
+        int sum = root.key;
 
         if(root.left != null) {
             sum += sumRecursive(root.left);
@@ -34,7 +57,7 @@ public class MyTree {
 
         while (!stack.isEmpty()) {
             MyTree node = stack.pop();
-            sum += node.value;
+            sum += node.key;
             if (node.right != null) {
                 stack.push(node.right);
             }
@@ -52,7 +75,7 @@ public class MyTree {
 
         while (!stack.isEmpty()) {
             MyTree node = stack.remove();
-            sum += node.value;
+            sum += node.key;
             if (node.right != null) {
                 stack.add(node.right);
             }

e91280e8ccb1e0ec175a6f43a6745201abaf2c9c
AlexeyTyan <alekseytyan45@gmail.com>
2021-02-02 16:05:02 +0300

    Testing

--- src/com/company/algorithms/sortings/Main.java
+++ src/com/company/algorithms/sortings/Main.java
@@ -1,13 +1,11 @@
 package com.company.algorithms.sortings;
 
-import com.company.algorithms.sortings.ArraySort;
 
 public class Main {
 
     public static void main(String[] args) {
 
 	    int[] a = new int[] {16,5,7,3,7,5,2,6,10,0,4,1};
-        int[] b = a.clone();
 
         ArraySort arraySort = new ArraySort();
         arraySort.bubbleSort(a.clone());
@@ -17,13 +15,24 @@ public class Main {
         arraySort.insertSort(a.clone());
         arraySort.shellSort(a.clone());
         arraySort.selectionSort(a.clone());
-        arraySort.quickSort(b, 0, b.length - 1);
-        for (int i = 0; i <b.length ; i++) {
-            System.out.print(b[i] + " ");
+        arraySort.mergeSort(a.clone()); // Итеративный подход
+
+        // В следующих двух сортировках (QuickSort & MergeSort) применяется
+        // парадигма "разделяй и властвуй", что подразумевает рекурсивный вызов.
+        // Поэтому мы не можем вывести массив в самом методе и выводим его здесь
+        int[] quickSortArray = a.clone();
+        arraySort.quickSort(quickSortArray, 0, quickSortArray.length - 1);
+        for (int j : quickSortArray) {
+            System.out.print(j + " ");
         }
         System.out.println(" Quick Sort");
 
-        arraySort.mergeSort(a.clone());
+        int[] simpleMergeArray = a.clone();
+        arraySort.simpleMergeSort(simpleMergeArray, 0, simpleMergeArray.length - 1);
+        for (int j: simpleMergeArray) {
+            System.out.print(j + " ");
+        }
+        System.out.println(" Simple Merge Sort");
 
 
     }

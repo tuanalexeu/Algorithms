76028770c46fdb31158b1f1c50581fd26be95952
AlexeyTyan <alekseytyan45@gmail.com>
2021-02-08 20:07:43 +0300

    Find Duplicates in array

---
+++ src/com/company/algorithms/problems/FindDuplicate.java
@@ -0,0 +1,14 @@
+package com.company.algorithms.problems;
+
+public class FindDuplicate {
+    public static int findDuplicate(int[] arr) {
+
+        int dupe = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            dupe = dupe ^ arr[i-1] ^ i;
+        }
+
+        return dupe;
+    }
+}

3a6d33e23b2242389ef59ec601ef5b0129a758b5
AlexeyTyan <alekseytyan45@gmail.com>
2021-02-08 20:55:51 +0300

    Check if string consist of only numbers

---
+++ src/com/company/algorithms/problems/strings/CheckNumbers.java
@@ -0,0 +1,19 @@
+package com.company.algorithms.problems.strings;
+
+public class CheckNumbers {
+    public static boolean checkNumbers(String s) {
+        for (Character c: s.toCharArray()) {
+            if(!isNumeric(c)) return false;
+        }
+        return true;
+    }
+
+    private static boolean isNumeric(char s) {
+        try {
+            Double.parseDouble(String.valueOf(s));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}

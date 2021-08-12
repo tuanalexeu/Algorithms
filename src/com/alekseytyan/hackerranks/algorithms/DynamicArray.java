98b8455f9886080f1777954aa776ecc48714c7a2
tuanalexeu <alekseytyan45@gmail.com>
2021-08-12 19:00:57 +0300

    Dynamic array : TODO

---
+++ src/com/alekseytyan/hackerranks/algorithms/DynamicArray.java
@@ -0,0 +1,25 @@
+package com.alekseytyan.hackerranks.algorithms;
+
+import java.util.Collections;
+import java.util.List;
+
+public class DynamicArray {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+     * Complete the 'dynamicArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+    public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
+        // Write your code here
+
+        return Collections.emptyList();
+    }
+}

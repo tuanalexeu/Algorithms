18b12668092a52660b4b50678889f0e3d07c2cf2
Aleksey Tyan <alekseytyan45@gmail.com>
2022-08-29 19:30:46 +0300

    Number of islands (TODO: Fix StackOverFlowError)

---
+++ src/com/alekseytyan/leetcode/NumberOfIslands.java
@@ -0,0 +1,71 @@
+package com.alekseytyan.leetcode;
+
+/**
+ * Given an m x n 2D binary grid 'grid' which represents a map of '1's (land) and '0's (water), return the number of islands.
+ *
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+ * You may assume all four edges of the grid are all surrounded by water.
+ *
+ * Example 1:
+ * Input: grid = [
+ *   ["1","1","1","1","0"],
+ *   ["1","1","0","1","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","0","0","0"]
+ * ]
+ * Output: 1
+ *
+ * Example 2:
+ * Input: grid = [
+ *   ["1","1","0","0","0"],
+ *   [,"1""1","0","0","0"],
+ *   ["0","0","1","0","0"],
+ *   ["0","0","0","1","1"]
+ * ]
+ * Output: 3
+ *
+ * Constraints:
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] is '0' or '1'.
+ */
+public class NumberOfIslands {
+    public static int numIslands(char[][] grid) {
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j] == '1') {
+                    // TODO: Fix StackOverFlowError
+                    visit(i, j, grid);
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    private static void visit(int i, int j, char[][] grid) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length) {
+            return;
+        }
+
+        grid[i][j] = '#'; // Отмечаем, что посетили данный узел.
+        visit(i - 1, j, grid);
+        visit(i + 1, j, grid);
+        visit(i, j - 1, grid);
+        visit(i, j + 1, grid);
+    }
+
+    public static void main(String[] args) {
+        char[][] input = new char[][] {
+            new char[] {'1','1','1','1','0'},
+            new char[] {'1','1','0','1','0'},
+            new char[] {'1','1','0','0','0'},
+            new char[] {'0','0','0','0','0'},
+        };
+        int output = numIslands(input);
+
+        System.out.println(output);
+    }
+}

c47b33a70933c820cefff21c9e5ff3c4cafa438f
Aleksey Tyan <alekseytyan45@gmail.com>
2022-09-05 18:11:09 +0300

    Rotate image

---
+++ src/com/alekseytyan/leetcode/RotateImage.java
@@ -0,0 +1,57 @@
+package com.alekseytyan.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+ * DO NOT allocate another 2D matrix and do the rotation.
+ *
+ * Example 1:
+ * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * Output: [[7,4,1],[8,5,2],[9,6,3]]
+ *
+ * Example 2:
+ * Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+ * Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+ *
+ *
+ * Constraints:
+ * n == matrix.length == matrix[i].length
+ * 1 <= n <= 20
+ * -1000 <= matrix[i][j] <= 1000
+ */
+public class RotateImage {
+    public static void rotate(int[][] matrix) {
+        reverse(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[i].length; j++) {
+                swap(i, j, j, i, matrix);
+            }
+        }
+    }
+
+    private static void reverse(int[][] matrix) {
+        for (int i = 0; i < matrix.length / 2; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                swap(i, j, matrix.length - (i + 1), j, matrix);
+            }
+        }
+    }
+
+    private static void swap(int i, int j, int i2, int j2, int[][] matrix) {
+        matrix[i][j] = matrix[i][j] ^ matrix[i2][j2];
+        matrix[i2][j2] = matrix[i2][j2] ^ matrix[i][j];
+        matrix[i][j] = matrix[i][j] ^ matrix[i2][j2];
+    }
+
+    public static void main(String[] args) {
+        int [][] input = new int[][] {
+            new int[] {1,2,3},
+            new int[] {4,5,6},
+            new int[] {7,8,9},
+        };
+        rotate(input);
+        Arrays.stream(input).forEach(arr -> System.out.println(Arrays.toString(arr)));
+    }
+}
